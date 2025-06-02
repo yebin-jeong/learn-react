9240265d8589bb4000a91ac0e64f979348778660
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-02 17:38:11 +0900

    todo 09

---
+++ workspace/ch01-buildup/todolist/09/src/components/Footer.js
@@ -0,0 +1,5 @@
+import Reaction from "../reaction.js";
+function Footer() {
+  return Reaction.createElement("footer", null, Reaction.createElement("p", null, "멋쟁이 사자처럼 FrontEnd BootCamp"));
+}
+export default Footer;
---
+++ workspace/ch01-buildup/todolist/09/src/pages/TodoList.js
@@ -0,0 +1,7 @@
+import Reaction from "../reaction.js";
+import TodoItem from "./TodoItem.js";
+function TodoList({ itemList, toggleDone, deleteItem }) {
+  const items = itemList.map((item) => TodoItem({ item, toggleDone, deleteItem }));
+  return Reaction.createElement("ul", { class: "todolist" }, items);
+}
+export default TodoList;
