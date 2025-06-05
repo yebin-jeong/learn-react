c5d0a8eeb32e54885e5873e072289f93da438acc
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-05 10:30:10 +0900

    09

--- workspace/ch02-start/vite/09/src/App.tsx
+++ workspace/ch02-start/vite/09/src/App.tsx
@@ -1,14 +1,25 @@
 import Title from "./components/Title";
 import TodoList from "./pages/todo/List";
 
+export interface TodoItem {
+  _id: number;
+  title: string;
+  done: boolean;
+}
+
 function App() {
+  const list: TodoItem[] = [
+    { _id: 1, title: "React 공부", done: false },
+    { _id: 2, title: "Javascript 프로젝트", done: true },
+    { _id: 3, title: "Javascript 공부", done: true },
+  ];
   return (
     <div id="app">
       <div>
         <Title />
         <Title titleName="타이틀1" />
         <Title titleName="타이틀2" />
-        <TodoList />
+        <TodoList list={list} />
       </div>
     </div>
   );
--- workspace/ch02-start/vite/09/src/pages/todo/List.tsx
+++ workspace/ch02-start/vite/09/src/pages/todo/List.tsx
@@ -1,11 +1,18 @@
-function TodoList() {
-  return (
-    <ul className="todolist">
-      <li>React 공부 - 진행중</li>
-      <li>Javascript 프로젝트 - 완료</li>
-      <li>Javascript 공부 - 완료</li>
-    </ul>
-  );
+import type { TodoItem } from "../../App";
+
+interface TodoListProps {
+  list: TodoItem[];
+}
+
+function TodoList({ list }: TodoListProps) {
+  const itemList = list.map((item) => {
+    return (
+      <li key={item._id}>
+        {item.title} - {item.done ? "완료" : "진행중"}
+      </li>
+    );
+  });
+  return <ul className="todolist">{itemList}</ul>;
 }
 
 export default TodoList;
