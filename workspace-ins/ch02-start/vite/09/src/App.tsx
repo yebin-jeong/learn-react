da110ed89a94b8fedc372ed0672e8a916f01f100
Kilyong Jeong <uzoolove@gmail.com>
2025-06-05 10:27:44 +0900

    🚧

--- workspace-ins/ch02-start/vite/09/src/App.tsx
+++ workspace-ins/ch02-start/vite/09/src/App.tsx
@@ -1,15 +1,27 @@
 import Title from "./components/Title";
 import TodoList from "./pages/todo/List";
 
+export interface TodoItem {
+  _id: number;
+  title: string;
+  done: boolean;
+}
+
 // Title, TodoList의 부모 컴포넌트
 function App(){
+  const list: TodoItem[] = [
+    { _id: 1, title: 'React 공부', done: false },
+    { _id: 2, title: 'Javascript 프로젝트', done: true },
+    { _id: 3, title: 'Javascript 공부', done: true }
+  ];
+  
   return (
     <div id="app">
       <div>
         <Title />
         <Title titleName="타이틀 1" />
         <Title titleName="타이틀 2" />
-        <TodoList />
+        <TodoList list={ list } />
       </div>
     </div>
   );
--- workspace-ins/ch02-start/vite/09/src/pages/todo/List.tsx
+++ workspace-ins/ch02-start/vite/09/src/pages/todo/List.tsx
@@ -1,10 +1,17 @@
+import type { TodoItem } from "../../App";
+
+interface TodoListProps {
+  list: TodoItem[]
+}
+
 // Todo 목록을 전달받아서 출력해야 함
-function TodoList(){
+function TodoList({ list }: TodoListProps){
+  const itemList = list.map(item => {
+    return <li key={ item._id }>{ item.title } - { item.done ? '완료' : '진행중' }</li>;
+  });
   return (
     <ul className="todolist">
-      <li>React 공부 - 진행중</li>
-      <li>Javascript 프로젝트 - 완료</li>
-      <li>Javascript 공부 - 완료</li>
+      { itemList }
     </ul>
   )
 }
