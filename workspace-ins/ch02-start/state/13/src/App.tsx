edda9d7d0813786e33738ff8139916b3dac1a804
Kilyong Jeong <uzoolove@gmail.com>
2025-06-09 13:29:56 +0900

    🚧

--- workspace-ins/ch02-start/state/13/src/App.tsx
+++ workspace-ins/ch02-start/state/13/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
+  console.log('App 렌더링');
+  const [ position, setPosition ] = useState({ x: 50, y: 150 });
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>13 상태관리 대상이 객체일 경우 주의 사항</h1>
+      <div className="container" onPointerMove={ event => {
+        console.log(event.pageX, event.pageY);
+
+        // 리렌더링이 되지 않음(객체의 속성만 수정)
+        // position.x = event.pageX;
+        // position.y = event.pageY;
+
+        // 레렌더링 됨(새로운 객체로 생성)
+        const newPosition = {
+          x: event.pageX,
+          y: event.pageY
+        };
+        setPosition(newPosition);
+      } }></div>
+      <div className="circle" style={{ transform: `translate(${position.x}px, ${position.y}px)` }}></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
