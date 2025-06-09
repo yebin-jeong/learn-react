221affef9c93731a033ec6d911c06d9e1b24335b
Kilyong Jeong <uzoolove@gmail.com>
2025-06-09 10:19:56 +0900

    🚧

--- workspace-ins/ch02-start/state/11/src/App.tsx
+++ workspace-ins/ch02-start/state/11/src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Message from "./components/Message";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>11 state 대신 컴포넌트 외부의 변수(모듈 스코프 변수) 사용시 문제점</h3>
+      <Message />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ workspace-ins/ch02-start/state/11/src/components/Message.tsx
@@ -0,0 +1,13 @@
+function Message(){
+  return (
+    <div>
+      <input type="text" />
+      <br />
+      <span>입력 메세지: </span>
+      <br />
+      <span>입력 횟수: </span>
+    </div>
+  );
+}
+
+export default Message;
