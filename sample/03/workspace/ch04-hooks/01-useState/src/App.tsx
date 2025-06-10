5ac154bf69590782b310201fa175f5e9365aa063
Kilyong Jeong <uzoolove@gmail.com>
2025-06-10 13:00:04 +0900

    🧪 03 추가

---
+++ sample/03/workspace/ch04-hooks/01-useState/src/App.tsx
@@ -0,0 +1,16 @@
+// TODO 1. 메세지를 입력하면 입력 메세지에 반영되도록 수정
+
+function App() {
+  return (
+    <>
+      <h1>01 useState - 상태 관리</h1>
+      <div>
+        <input type="text" value="hello" />
+        <br />
+        <span>입력 메세지: hello</span>
+      </div>
+    </>
+  );
+}
+
+export default App
