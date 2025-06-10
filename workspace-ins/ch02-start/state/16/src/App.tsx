ea64966846387c7b842dc68168265df3f6066b61
Kilyong Jeong <uzoolove@gmail.com>
2025-06-10 14:47:29 +0900

    ✨

--- workspace-ins/ch02-start/state/16/src/App.tsx
+++ workspace-ins/ch02-start/state/16/src/App.tsx
@@ -20,6 +20,9 @@ const cellphoneExp = /^(01[016789]{1})[0-9]{3,4}[0-9]{4}$/;
 function App() {
 
   const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    mode: 'onSubmit', // default: onSubmit, 최초 검증 시점
+    reValidateMode: 'onBlur', // default: onChange, 재검증 시점
+    criteriaMode: 'firstError', // default: firstError, errors 객체에 첫 오류 하나만 포함하거나 전부 포함
     defaultValues: {
       name: '',
       email: '',
@@ -27,7 +30,9 @@ function App() {
     }
   });
 
-  // 검증 완료시 실행할 코드
+  console.log(errors);
+
+  // 검증 완료후에 실행할 코드
   const onSubmitHandler = (user: User) => {
     console.log('서버에 전송...', user);
   };
@@ -35,24 +40,44 @@ function App() {
   return (
     <>
       <h1>16 회원가입 입력값 검증 (feat. react-hook-form)</h1>
-      <form onSubmit={onSubmitHandler}>
+      <form onSubmit={ handleSubmit(onSubmitHandler) }>
         <label htmlFor="name">*이름</label>
         <input id="name"
           { ...register('name', {
-            required: ''
+            required: '이름을 입력하세요.',
+            minLength: {
+              value: 2,
+              message: '2글자 이상 입력하세요.'
+            },
+            pattern: {
+              value: /^[^\d]*$/, // 숫자가 포함되지 않도록 설정
+              message: '숫자는 입력할 수 없습니다.'
+            }
           }) }
         /><br />
         <div style={errorStyle}>{ errors.name?.message }</div>
 
         <label htmlFor="email">*이메일</label>
         <input id="email"
-          { ...register('email', {}) }
+          { ...register('email', {
+            required: '이메일을 입력하세요.',
+            pattern: {
+              value: emailExp,
+              message: '이메일 양식에 맞지 않습니다.'
+            }
+          }) }
         /><br />
         <div style={errorStyle}>{ errors.email?.message }</div>
 
         <label htmlFor="cellphone">*휴대폰</label>
         <input id="cellphone"
-          { ...register('email', {}) }
+          { ...register('cellphone', {
+            required: '휴대폰 번호를 입력하세요.',
+            pattern: {
+              value: cellphoneExp,
+              message: '휴대폰 형식에 맞지 않습니다.'
+            }
+          }) }
         /><br />
         <div style={errorStyle}>{ errors.cellphone?.message }</div>
 
@@ -60,12 +85,12 @@ function App() {
       </form>
 
       <p>
-        이름: {user.name}<br />
-        이메일: {user.email}<br />
-        휴대폰: {user.cellphone}<br />
+        이름: { watch('name') }<br />
+        이메일: { watch('email') }<br />
+        휴대폰: { watch('cellphone') }<br />
       </p>
     </>
-  );  
+  );
 }
 
 export default App
