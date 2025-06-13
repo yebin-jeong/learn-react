021fc650c6f1c57a1470d974cb942be309d29a4b
Kilyong Jeong <uzoolove@gmail.com>
2025-06-13 11:09:24 +0900

    ✨

--- workspace-ins/ch04-hooks/05-useMemo/src/App.tsx
+++ workspace-ins/ch04-hooks/05-useMemo/src/App.tsx
@@ -1,3 +1,5 @@
+import { useMemo, useState } from "react";
+
 // 지정한 수가 소수인지 여부를 반환
 const isPrime = function(num: number){
   console.time('소요 시간');
@@ -21,17 +23,26 @@ const isPrime = function(num: number){
 function App() {
   console.log('App 렌더링.');
 
+  const [ name, setName ] = useState('GD');
+  const [ num, setNum ] = useState(1);
+
   // TODO name이 바뀔때 리렌더링이 필요하지만 소수 계산은 다시 할 필요 없음
   // num이 바뀔때만 다시 계산하고 num이 바뀌지 않으면 메모이제이션 된 값을 반환해야 함
-  
+  const prime = useMemo(() => isPrime(num), [num]);
   
   return (
     <>
       <h1>05 useMemo - 함수의 반환값을 memoize</h1>
       <div>
-        <input type="text" />가 좋아하는 숫자: 
-        <input type="number" min="1" max="1000000007" />
-        <div>GD가 좋아하는 숫자 1000000007: 소수가 <span style={{color: 'blue'}}>맞습니다.</span><span style={{color: 'red'}}>아닙니다.</span></div>
+        <input type="text" value={ name } onChange={ e => setName(e.target.value) } />가 좋아하는 숫자: 
+        <input type="number" min="1" max="1000000007" value={ num || '' } onChange={ e => setNum(Number(e.target.value)) } />
+        <div>{ name }가 좋아하는 숫자 { num }: 소수가 
+          { prime ? 
+              <span style={{color: 'blue'}}> 맞습니다.</span> 
+            : 
+              <span style={{color: 'red'}}> 아닙니다.</span> 
+          }
+        </div>
       </div>
     </>
   );
