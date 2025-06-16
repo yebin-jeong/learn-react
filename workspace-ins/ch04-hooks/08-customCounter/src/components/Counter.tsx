1a4e29094420a4f15c44d42e6d9f7b06e30bec5b
Kilyong Jeong <uzoolove@gmail.com>
2025-06-16 13:15:34 +0900

    ✨

--- workspace-ins/ch04-hooks/08-customCounter/src/components/Counter.tsx
+++ workspace-ins/ch04-hooks/08-customCounter/src/components/Counter.tsx
@@ -1,5 +1,5 @@
+import useCounter from "@/hooks/useCounter";
 import Button from "@components/Button";
-import { useRef, useState } from "react";
 
 interface CounterProps {
   children: string;
@@ -10,25 +10,7 @@ function Counter({ children='1' }: CounterProps){
   console.log('\tCounter 호출됨');
 
   const initCount = Number(children);
-
-  // TODO 커스텀 훅으로 수정
-  
-  
- 
-  // 카운터 감소
-  const handleDown = () => {
-    setCount(count - stepRef.current);
-  };
-
-  // 카운터 증가
-  const handleUp = () => {
-    setCount(count + stepRef.current);
-  };
-
-  // 카운터 초기화
-  const handleReset = () => {
-    setCount(0);
-  };
+  const { count, stepRef, down, up, reset } = useCounter(initCount);
 
   return (
     <div id="counter">
@@ -39,9 +21,9 @@ function Counter({ children='1' }: CounterProps){
         value={ stepRef.current }
         onChange={ (event) => stepRef.current = Number(event.target.value) }
       />
-      <Button color="red" onClick={ handleDown }>-_-</Button>
-      <Button type="reset" onClick={ handleReset }>0_0</Button>
-      <Button type="submit" color="blue" onClick={ handleUp }>+_+</Button>
+      <Button color="red" onClick={ down }>-_-</Button>
+      <Button type="reset" onClick={ reset }>0_0</Button>
+      <Button type="submit" color="blue" onClick={ up }>+_+</Button>
       <span>{ count }</span>
     </div>
   );
