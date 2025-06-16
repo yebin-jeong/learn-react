c0dae85d6b1b2d2a922d0d628eb85a13c71da088
Kilyong Jeong <uzoolove@gmail.com>
2025-06-16 14:51:30 +0900

    🚧

--- workspace-ins/ch04-hooks/09-fetch/src/App.tsx
+++ workspace-ins/ch04-hooks/09-fetch/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 
 // API 서버 엔드포인트 주소 상수 정의
 const API_SERVER = 'https://fesp-api.koyeb.app/todo';
@@ -45,16 +46,44 @@ function App() {
   */
 
   // Todo 목록을 저장할 상태 (초기값: null)
+  const [ data, setData ] = useState<TodoListRes | null>(null);
   
   // 에러 메시지를 저장할 상태 (초기값: null)
+  const [ error, setError ] = useState<Error | null>(null);
   
   // 로딩 상태를 저장할 상태 (초기값: false)
+  const [ isLoading, setIsLoading ] = useState(false);
   
   // Todo API 서버에 데이터를 요청하는 비동기 함수
   const fetchTodo = async (fetchParams: FetchParams) => {
     console.log('fetchTodo 함수 호출됨', fetchParams);
-
+    try {
+      // 로딩 상태를 true로 설정
+      setIsLoading(true);
+      const res = await fetch(API_SERVER + fetchParams.url);
+      console.log('서버 응답', res);
+      const jsonRes: ResData = await res.json();
+      console.log('파싱된 데이터', jsonRes);
+      if(jsonRes.ok){
+        setData(jsonRes);
+      }else{
+        
+      }
+      
+    } catch(err){
+      console.error(err);
+    } finally {
+      // 성공/실패 여부과 관계없이 로딩 상태를 false로 설정
+      setIsLoading(false);
+    }
   };
+
+  // 컴포넌트가 마운트 된 후에 Todo 목록 조회
+  useEffect(() => {
+    console.log('마운트 후에 한번만 호출됨.');
+    const params = { url: '/todolist' };
+    fetchTodo(params);
+  }, []); // 빈 배열을 전달해서 마운트시 한번만 실행되도록 설정
 
   return (
     <>
@@ -62,15 +91,14 @@ function App() {
       <h2>할일 목록</h2>
 
       {/* 로딩중일 때 로딩중 메시지 표시 */}
-      <p>로딩중...</p>
-
+      { isLoading && <p>로딩중...</p> }
+      
       {/* 에러가 있을 경우 빨간색으로 에러 메시지 표시 */}
       <p style={{ color: 'red' }}>에러 메세지</p>
           
       {/* 서버에서 받은 Todo 목록을 렌더링 */}
       <ul>
-        <li>자바스크립트 공부</li>
-        <li>리액트 공부</li>
+        { data?.items.map() }
       </ul>
     </>
   );
