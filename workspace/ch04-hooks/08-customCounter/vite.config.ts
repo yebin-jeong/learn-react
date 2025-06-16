99fb528bbec00ed93f46e994e474f1b77af1a00a
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-16 13:28:08 +0900

    counter 08

--- workspace/ch04-hooks/08-customCounter/vite.config.ts
+++ workspace/ch04-hooks/08-customCounter/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: "/src" },
@@ -12,4 +18,4 @@ export default defineConfig({
       { find: "@hooks", replacement: "/src/hooks" },
     ],
   },
-})
+});
