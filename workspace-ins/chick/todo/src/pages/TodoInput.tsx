8234eb58c22260abe9a656f1dd43dedc273d082f
Kilyong Jeong <uzoolove@gmail.com>
2025-06-23 17:58:10 +0900

    🚧 할일 추가 이벤트 핸들러 작성

--- workspace-ins/chick/todo/src/pages/TodoInput.tsx
+++ workspace-ins/chick/todo/src/pages/TodoInput.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
 function TodoInput() {
+
+  console.log('### TodoInput 호출됨.');
+
+  // 제어 컴포넌트 1. state 정의
+  const [ title, setTitle ] = useState('');
+
+  // 추가 버튼 클릭 이벤트 처리
+  const handleAdd = () => {
+    console.log(`${title} 추가`);
+    
+  };
+
   return (
     <div className="todoinput">
-      <input type="text" autoFocus />
-      <button type="button">추가</button>
+      {/* 제어 컴포넌트 2. value를 state로 지정 */}
+      {/* 제어 컴포넌트 3. onChange 이벤트에서 setState 호출 */}
+      <input type="text" value={ title } onChange={ e => setTitle(e.target.value) } autoFocus />
+      <button type="button" onClick={ handleAdd }>추가</button>
     </div>
   );
 }
--- workspace-ins/chick/todo/src/pages/TodoList.tsx
+++ workspace-ins/chick/todo/src/pages/TodoList.tsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 
 function TodoList() {
 
   // 샘플 목록
-  const itemList = [
+  const sampleItemList = [
     { _id: 1, title: '자바스크립트 공부', done: true },
     { _id: 2, title: 'JS 프로젝트', done: true },
     { _id: 3, title: 'React 공부', done: false },
   ];
 
+  // useState가 처음으로 호출될 때 itemList 값은 sampleItemList가 된다.
+  // useState가 두번째 또는 그 이상 호출될 때는 itemList 값은 setItemList()에 전달한 newItemList가 된다.
+  const [ itemList, setItemList ] = useState(sampleItemList);
+  
+  // 할일 삭제
+  const deleteItem = (_id: number) => {
+    // _id: 2일 경우 1, 3만 담은 새로운 배열을 반환
+    const newItemList = itemList.filter((item) => item._id !== _id);
+    setItemList(newItemList);
+  };
+
   const liList = itemList.map((item) => {
     return (
-      <TodoItem key={ item._id } item={ item } hello="world" />
+      <TodoItem key={ item._id } item={ item } deleteItem={ deleteItem } />
     );
   });
 
