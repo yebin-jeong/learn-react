cacd5bd4e1f807ad0a39114a6ab8f0df564819f7
Kilyong Jeong <uzoolove@gmail.com>
2025-06-23 15:27:33 +0900

    🚧 댓글 목록 출력

--- workspace-ins/ch09-ajax/01-board-fetch/src/pages/board/CommentList.tsx
+++ workspace-ins/ch09-ajax/01-board-fetch/src/pages/board/CommentList.tsx
@@ -1,13 +1,18 @@
 import CommentNew from "@/pages/board/CommentNew";
+import type { ReplyType } from "@/types/BoardType";
 
-function CommentList() {
+interface PropType {
+  replies: ReplyType[];
+}
+
+function CommentList({ replies=[] }: PropType) {
+  const replyList = replies.map(reply => <li key={ reply._id }>{ reply.content }</li>);
+
   return (
     <>
       <h3>댓글 목록</h3>
       <ul>
-        <li>3번 댓글</li>
-        <li>2번 댓글</li>
-        <li>1번 댓글</li>
+        { replyList }
       </ul>
       <CommentNew />
     </>
