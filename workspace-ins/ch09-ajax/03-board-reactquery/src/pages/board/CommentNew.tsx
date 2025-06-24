c0b9e09a061a928f491e48ed54e4fd033ab8e5f0
Kilyong Jeong <uzoolove@gmail.com>
2025-06-24 14:51:22 +0900

    🚧

--- workspace-ins/ch09-ajax/03-board-reactquery/src/pages/board/CommentNew.tsx
+++ workspace-ins/ch09-ajax/03-board-reactquery/src/pages/board/CommentNew.tsx
@@ -1,4 +1,5 @@
 import useAxiosInstance from "@/hooks/useAxiosInstance";
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 
 function CommentNew() {
@@ -9,24 +10,19 @@ function CommentNew() {
   // axios instance
   const axios = useAxiosInstance();
 
-  // API 서버에 댓글 등록 요청
-  const requestAddComment = async (formData: FormData) => {
-    try{
-      const response = await axios.post('/posts/1/replies', formData);
-
+  const { mutate } = useMutation({
+    mutationFn: (formData: FormData) => axios.post('/posts/1/replies', formData),
+    onSuccess() {
       // TODO 댓글 등록 후 댓글 목록 갱신(requestCommentList()를 props로 전달받아서 호출)
 
-    }catch(err){
-      // alert('게시물 상세 조회에 실패했습니다.\n잠시 후 다시 요청하시기 바랍니다.');
-      console.error(err);
     }
-  };
+  });
 
   const handleAddComment = (event: React.FormEvent<HTMLFormElement>) => {
     // TODO 중복 요청 방지(요청 시작 전에 버튼 비활성화, 응답 완료 후에 버튼 활성화)
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    requestAddComment(formData);
+    mutate(formData);
   };
 
   return (
