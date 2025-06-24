b89b9c29d3262692be6fb924e87d79dea269549d
Kilyong Jeong <uzoolove@gmail.com>
2025-06-24 13:23:29 +0900

    📝

--- workspace-ins/ch09-ajax/02-board-axios/src/pages/board/CommentList.tsx
+++ workspace-ins/ch09-ajax/02-board-axios/src/pages/board/CommentList.tsx
@@ -17,8 +17,15 @@ function CommentList() {
   // axios instance
   const axios = useAxiosInstance();
 
-  // API 서버에 1번 게시물의 댓글 목록을 fetch() 요청으로 보낸다.
+  // API 서버에 1번 게시물의 댓글 목록을 axios() 요청으로 보낸다.
   const requestCommentList = async () => {
+    // TODO 작업이 실패하면 자동으로 재시도하기(catch 블럭에서 지정한 횟수만큼 requestCommentList() 호출)
+    // TODO 다른탭이나 앱에서 작업 후에 돌아오면 데이터 자동으로 갱신하기
+    //      - document에 visibilitychange 이벤트로 브라우저의 가시성 변경을 감지. 
+    //      - window에 focus 이벤트로 브라우저 탭의 포커스 변경을 감지해서 requestCommentList() 호출)
+    // TODO 일정 시간동안은 캐시해서 서버 호출 횟수 줄이기(캐시 관련 로직 작성)
+    // TODO 주기적으로 호출해서 데이터를 자동으로 갱신하기(setInterval() 함수로 일정 시간마다 requestCommentList() 호출)
+
     try{
       // 로딩 상태를 true로 지정
       setIsLoading(true);
@@ -26,8 +33,8 @@ function CommentList() {
       const response = await axios.get<ReplyListResType>('/posts/1/replies', {
         params: {
           delay: 1000,
-          page: 3,
-          limit: 10,
+          // page: 3,
+          // limit: 10,
         }
       });
 
--- workspace-ins/ch09-ajax/02-board-axios/src/pages/board/CommentNew.tsx
+++ workspace-ins/ch09-ajax/02-board-axios/src/pages/board/CommentNew.tsx
@@ -13,6 +13,9 @@ function CommentNew() {
   const requestAddComment = async (formData: FormData) => {
     try{
       const response = await axios.post('/posts/1/replies', formData);
+
+      // TODO 댓글 등록 후 댓글 목록 갱신(requestCommentList()를 props로 전달받아서 호출)
+
     }catch(err){
       // alert('게시물 상세 조회에 실패했습니다.\n잠시 후 다시 요청하시기 바랍니다.');
       console.error(err);
@@ -20,8 +23,8 @@ function CommentNew() {
   };
 
   const handleAddComment = (event: React.FormEvent<HTMLFormElement>) => {
+    // TODO 중복 요청 방지(요청 시작 전에 버튼 비활성화, 응답 완료 후에 버튼 활성화)
     event.preventDefault();
-
     const formData = new FormData(event.currentTarget);
     requestAddComment(formData);
   };
