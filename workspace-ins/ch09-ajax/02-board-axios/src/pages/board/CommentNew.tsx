777de5bae0d7129fc96b302484723f2ff4abf519
Kilyong Jeong <uzoolove@gmail.com>
2025-06-24 11:45:11 +0900

    🚧 댓글 등록 추가

--- workspace-ins/ch09-ajax/02-board-axios/src/pages/board/CommentNew.tsx
+++ workspace-ins/ch09-ajax/02-board-axios/src/pages/board/CommentNew.tsx
@@ -1,3 +1,4 @@
+import useAxiosInstance from "@/hooks/useAxiosInstance";
 import { useState } from "react";
 
 function CommentNew() {
@@ -5,19 +6,13 @@ function CommentNew() {
   // 댓글 내용 저장
   const [content, setContent] = useState('');
 
+  // axios instance
+  const axios = useAxiosInstance();
+
   // API 서버에 댓글 등록 요청
   const requestAddComment = async (formData: FormData) => {
     try{
-      // FormData를 일반 Object로 변환
-      const jsonBody = Object.fromEntries(formData.entries());
-      await fetch('https://fesp-api.koyeb.app/market/posts/1/replies?delay=1000', {
-        headers: {
-          'Client-Id': 'openmarket',
-          'Content-Type': 'application/json', // 요청 바디의 데이터 타입을 서버에 json이라고 알림
-        },
-        method: 'POST',
-        body: JSON.stringify(jsonBody), // 객체를 JSON 문자열로 변환
-      });
+      const response = await axios.post('/posts/1/replies', formData);
     }catch(err){
       // alert('게시물 상세 조회에 실패했습니다.\n잠시 후 다시 요청하시기 바랍니다.');
       console.error(err);
