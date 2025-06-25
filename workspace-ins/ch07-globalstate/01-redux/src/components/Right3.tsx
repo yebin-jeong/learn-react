96ad77360541ffe227f2060f3f8942fa9030767c
Kilyong Jeong <uzoolove@gmail.com>
2025-06-25 16:41:23 +0900

    ✨ redux 완료

--- workspace-ins/ch07-globalstate/01-redux/src/components/Right3.tsx
+++ workspace-ins/ch07-globalstate/01-redux/src/components/Right3.tsx
@@ -1,13 +1,18 @@
+import counterActionCreator from '@/redux/counterActionCreator';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Right3() {
   useEffect(()=>{
     console.log('#### Right3 렌더링.');
   });
+
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      <button onClick={ () => dispatch(counterActionCreator.countUp(3)) }>+1</button>
     </div>
   );
 }
