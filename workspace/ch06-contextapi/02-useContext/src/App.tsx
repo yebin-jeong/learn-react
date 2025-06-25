1fd98095ed268be88cf00a637be2d114d9a30b57
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-25 13:08:51 +0900

    context api

--- workspace/ch06-contextapi/02-useContext/src/App.tsx
+++ workspace/ch06-contextapi/02-useContext/src/App.tsx
@@ -1,13 +1,22 @@
-import { useEffect } from 'react';
-import Left1 from '@/components/Left1';
-import Right1 from '@/components/Right1';
-import './App.css';
+import { useEffect, useState } from "react";
+import Left1 from "@/components/Left1";
+import Right1 from "@/components/Right1";
+import "./App.css";
+import { CounterContext } from "@/contexts/CounterContext";
 
 function App() {
-
-  useEffect(()=>{
-    console.log('# App 렌더링.');
+  useEffect(() => {
+    // TODO count가 수정되어도 불필요한 렌더링 발생하지 않도록 CounterContext로 상태 관리 로직 이동
+    console.log("# App 렌더링.");
   });
+
+  const [count, setCount] = useState(3);
+
+  const countUp = (step: number) => {
+    setCount(count + step);
+  };
+
+  const value = { count, countUp };
 
   return (
     <>
@@ -15,8 +24,11 @@ function App() {
       <div id="container">
         <h1>App</h1>
         <div id="grid">
-          <Left1 />
-          <Right1 />
+          {/* 3. Context 제공하기 */}
+          <CounterContext value={value}>
+            <Left1 />
+            <Right1 />
+          </CounterContext>
         </div>
       </div>
     </>
