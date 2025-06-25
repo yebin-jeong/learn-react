16a25f5e7c1e8a1a4cc0423322f0f988347e84d2
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-25 10:22:36 +0900

    인터셉터 추가

--- workspace/ch09-ajax/02-board-axios/src/pages/board/CommentNew.tsx
+++ workspace/ch09-ajax/02-board-axios/src/pages/board/CommentNew.tsx
@@ -16,7 +16,6 @@ function CommentNew() {
       // TODO 댓글 등록 후 댓글 목록 갱신(requestCommentList()를 props로 전달받아서 호출)
     } catch (err) {
       // alert('게시물 상세 조회에 실패했습니다.\n잠시 후 다시 요청하시기 바랍니다.');
-      console.error(err);
     }
   };
 
