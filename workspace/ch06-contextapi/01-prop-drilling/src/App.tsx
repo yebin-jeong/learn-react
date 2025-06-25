c69bc31b83ccbf2120146a9ecb213b682280e2d5
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-25 11:21:02 +0900

    프롭 드릴링

--- workspace/ch06-contextapi/01-prop-drilling/src/App.tsx
+++ workspace/ch06-contextapi/01-prop-drilling/src/App.tsx
@@ -1,13 +1,18 @@
-import { useEffect } from 'react';
-import Left1 from '@/components/Left1';
-import Right1 from '@/components/Right1';
-import './App.css';
+import { useEffect, useState } from "react";
+import Left1 from "@/components/Left1";
+import Right1 from "@/components/Right1";
+import "./App.css";
 
 function App() {
-
-  useEffect(()=>{
-    console.log('# App 렌더링.');
+  useEffect(() => {
+    console.log("# App 렌더링.");
   });
+
+  const [count, setCount] = useState(3);
+
+  const countUp = (step: number) => {
+    setCount(count + step);
+  };
 
   return (
     <>
@@ -15,8 +20,8 @@ function App() {
       <div id="container">
         <h1>App</h1>
         <div id="grid">
-          <Left1 />
-          <Right1 />
+          <Left1 count={count} />
+          <Right1 countUp={countUp} />
         </div>
       </div>
     </>
