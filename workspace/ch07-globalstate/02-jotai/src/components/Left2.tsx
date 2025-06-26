afe25f0e3a907ece20e81380517f967fc49c19bc
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-26 11:18:58 +0900

    jotai

---
+++ workspace/ch07-globalstate/02-jotai/src/components/Left2.tsx
@@ -0,0 +1,22 @@
+import Left3 from "@/components/Left3";
+import { countAtom } from "@/jotai/atoms";
+import { useAtomValue } from "jotai";
+import { useEffect } from "react";
+
+function Left2() {
+  useEffect(() => {
+    console.log("### Left2 렌더링.");
+  });
+
+  const count = useAtomValue(countAtom);
+
+  return (
+    <div>
+      <h2>Left2</h2>
+      <span>{count}</span>
+      <Left3 />
+    </div>
+  );
+}
+
+export default Left2;
---
+++ workspace/ch07-globalstate/02-jotai/src/components/Left3.tsx
@@ -0,0 +1,20 @@
+import { countAtom } from "@/jotai/atoms";
+import { useAtomValue } from "jotai";
+import { useEffect } from "react";
+
+function Left3() {
+  useEffect(() => {
+    console.log("#### Left3 렌더링.");
+  });
+
+  const count = useAtomValue(countAtom);
+
+  return (
+    <div>
+      <h3>Left3</h3>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+export default Left3;
