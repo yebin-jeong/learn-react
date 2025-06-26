a37d7a8acf4e06d5f7e1f4f105ca44ab3f7c0562
Kilyong Jeong <uzoolove@gmail.com>
2025-06-26 16:39:20 +0900

    ✨ tailwind 완료

--- workspace-ins/ch08-css/04-tailwind/src/App.tsx
+++ workspace-ins/ch08-css/04-tailwind/src/App.tsx
@@ -6,8 +6,8 @@ function App(){
       <h1>04 Tailwind CSS</h1>
 
       <Button type="button">그냥 버튼</Button>
-      <Button type="button" bg="blue" color="red">파란 버튼</Button>
-      <Button type="submit" bg="gray" color="blue">submit 버튼</Button>
+      <Button type="button" bg="yellow" color="red">파란 버튼</Button>
+      <Button type="submit" bg="red" color="blue">submit 버튼</Button>
 
     </>
   );
--- workspace-ins/ch08-css/04-tailwind/src/components/ui/Button.tsx
+++ workspace-ins/ch08-css/04-tailwind/src/components/ui/Button.tsx
@@ -1,13 +1,24 @@
-import styles from './Button.module.css';
-
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
   color?: string;
-  bg?: string;
+  bg?: 'gray' | 'red' | 'yellow';
+  size?: 'sm' | 'md' | 'lg';
 }
 
-function Button({ children, bg, color, ...rest }: ButtonProps){
+function Button({ children, bg="gray", size="md", ...rest }: ButtonProps){
+  const btnColor = {
+    gray: 'bg-gray-500',
+    red: 'bg-red-500',
+    yellow: 'bg-yellow-500',
+  };
+
+  const btnSize = {
+    sm: 'py-1 px-2 text-sm',
+    md: 'py-2 px-4 text-base',
+    lg: 'py-2 px-6 text-lg',
+  };
+
   return (
-    <button className={`${styles.button} ${styles[`bg-${bg}-text-${color}`]}`} {...rest}>{ children }</button>
+    <button className={`${btnColor[bg]} border-0 text-white py-1.5 px-4 text-center my-1 mx-0.5 rounded-md cursor-pointer hover:bg-green-700 ${btnSize[size]}`} {...rest}>{ children }</button>
   );
 }
 
