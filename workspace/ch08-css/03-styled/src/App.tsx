54cba7a714d1b1bbbacb51c16e583f60ab3af249
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-26 16:11:32 +0900

    styled

--- workspace/ch08-css/03-styled/src/App.tsx
+++ workspace/ch08-css/03-styled/src/App.tsx
@@ -1,14 +1,17 @@
 import Button from "@/components/ui/Button";
 
-function App(){
+function App() {
   return (
     <>
       <h1>03 Styled Components</h1>
 
       <Button type="button">그냥 버튼</Button>
-      <Button type="button" bg="blue" color="red">취소 버튼</Button>
-      <Button type="submit" bg="gray" color="blue">submit 버튼</Button>
-
+      <Button type="button" variant="cancel">
+        취소 버튼
+      </Button>
+      <Button type="submit" variant="confirm">
+        submit 버튼
+      </Button>
     </>
   );
 }
--- workspace/ch08-css/03-styled/src/components/ui/Button.tsx
+++ workspace/ch08-css/03-styled/src/components/ui/Button.tsx
@@ -1,14 +1,46 @@
-import styles from './Button.module.css';
+import styled from "styled-components";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   color?: string;
   bg?: string;
+  variant?: "basic" | "cancel" | "confirm";
 }
 
-function Button({ children, bg, color, ...rest }: ButtonProps){
-  return (
-    <button className={`${styles.button} ${styles[`bg-${bg}-text-${color}`]}`} {...rest}>{ children }</button>
-  );
+const BasicButtonStyle = styled.button<ButtonProps>`
+  background-color: ${(props) => props.bg || "gray"};
+  border: none;
+  color: ${(props) => props.color || "black"};
+  padding: 6px 18px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 6px;
+`;
+
+const CancelButtonStyle = styled(BasicButtonStyle)`
+  background-color: red;
+  color: white;
+`;
+
+const ConfirmButtonStyle = styled(BasicButtonStyle)`
+  background-color: blue;
+  color: white;
+`;
+
+function Button({ children, variant = "basic", ...rest }: ButtonProps) {
+  switch (variant) {
+    case "cancel":
+      return <CancelButtonStyle {...rest}>{children}</CancelButtonStyle>;
+
+    case "confirm":
+      return <ConfirmButtonStyle {...rest}>{children}</ConfirmButtonStyle>;
+    case "basic":
+    default:
+      return <BasicButtonStyle {...rest}>{children}</BasicButtonStyle>;
+  }
 }
 
 export default Button;
