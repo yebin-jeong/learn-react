95cbd8b4563884dab26fad0978c99f5d5c46f124
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-26 16:40:38 +0900

    tailwind

--- workspace/ch08-css/04-tailwind/src/App.tsx
+++ workspace/ch08-css/04-tailwind/src/App.tsx
@@ -1,14 +1,17 @@
 import Button from "@/components/ui/Button";
 
-function App(){
+function App() {
   return (
     <>
       <h1>04 Tailwind CSS</h1>
 
       <Button type="button">그냥 버튼</Button>
-      <Button type="button" bg="blue" color="red">파란 버튼</Button>
-      <Button type="submit" bg="gray" color="blue">submit 버튼</Button>
-
+      <Button type="button" bg="yellow" color="red">
+        파란 버튼
+      </Button>
+      <Button type="submit" bg="red" color="blue">
+        submit 버튼
+      </Button>
     </>
   );
 }
--- workspace/ch08-css/04-tailwind/vite.config.ts
+++ workspace/ch08-css/04-tailwind/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: [
       { find: "@", replacement: "/src" },
@@ -21,4 +20,4 @@ export default defineConfig({
       { find: "@zustand", replacement: "/src/zustand" },
     ],
   },
-})
+});
