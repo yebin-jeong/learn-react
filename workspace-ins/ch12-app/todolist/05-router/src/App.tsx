1ba4fd51c3829ff7f7efde50ab8ec7cf416fc303
Kilyong Jeong <uzoolove@gmail.com>
2025-06-30 10:56:42 +0900

    ✨ suspense 적용

--- workspace-ins/ch12-app/todolist/05-router/src/App.tsx
+++ workspace-ins/ch12-app/todolist/05-router/src/App.tsx
@@ -2,10 +2,13 @@ import { RouterProvider } from "react-router";
 // import router from "./routes";
 import router from "./routes-lazy";
 import './App.css';
+import { Suspense } from "react";
 
 function App() {
   return (
-    <RouterProvider router={ router } />
+    <Suspense fallback={ <p>Loading...</p> }>
+      <RouterProvider router={ router } />
+    </Suspense>
   );
 }
 
