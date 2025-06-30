42e0c2daf76fa37b6b6602cd133aff3fc2a49e62
Kilyong Jeong <uzoolove@gmail.com>
2025-06-30 09:50:52 +0900

    ✨ lazy loading 추가

--- workspace-ins/ch12-app/todolist/05-router/src/App.tsx
+++ workspace-ins/ch12-app/todolist/05-router/src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router";
-import router from "./routes";
+// import router from "./routes";
+import router from "./routes-lazy";
 import './App.css';
 
 function App() {
